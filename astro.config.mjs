97712c037f282b3035282dba339914684f7df0ee
pbourgeois <pbcomweb@gmail.com>
2022-12-27 08:30:43 -0500

    Don’t set a base parameter if

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bourpie.github.io',
-    base: '/mixed-meteorite',
   })
