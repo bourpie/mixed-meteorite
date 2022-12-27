9fd134812dcafc5fdab508cdd01d0059263a8049
pbourgeois <pbcomweb@gmail.com>
2022-12-27 09:13:07 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bourpie.github.io',
+    base: '/mixed-meteorite',
   })
